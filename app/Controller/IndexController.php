971d94ac9c0f9541d9d1f52c8d30b933b0cbca39
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2026-02-22 18:18:28 -0300

    chore: edit the index controller to show status of application and database

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -12,16 +12,17 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Hyperf\DbConnection\Db;
+
 class IndexController extends AbstractController
 {
     public function index()
     {
-        $user = $this->request->input('user', 'Hyperf');
-        $method = $this->request->getMethod();
+        $result = Db::select('SELECT DATABASE() as db');
 
         return [
-            'method' => $method,
-            'message' => "Hello {$user}.",
+            'status' => 'ok',
+            'database' => $result[0]->db ? 'ok' : 'down',
         ];
     }
 }
